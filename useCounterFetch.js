dcf5fcca2bf6c54ddecd493afde9005a96b4c67e
Juan Ignacio <juaniclausi@gmail.com>
2021-05-15 21:13:15 -0300

    Primer commit

---
+++ useCounterFetch.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export const useCounterFetch = ( initialState = 10 ) => {
+
+    const [counter, setCounter] = useState(initialState);
+
+    const increment = () => { setCounter( counter + 1) }
+    const decrement = () => { setCounter( counter - 1) }
+    const reset = () => { setCounter( initialState ) }
+
+    return {       
+        counter,
+        increment,
+        decrement,
+        reset
+    };
+
+}
